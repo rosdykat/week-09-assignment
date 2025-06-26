5c811c66fbbec2f8cea90b584c0d5072db26a15d
Ross Fletcher <rossfletcher108@gmail.com>
2025-06-26 11:53:12 +0100

    added readme and initial setup

---
+++ src/app/sign-up/[[...sign-up]]/createProfile/page.tsx
@@ -0,0 +1,12 @@
+// TODO: Allow the user to add their personal information
+// clerkId does not exist unti lthe user signs up
+
+export default function CreateProfilePage() {
+  return (
+    <>
+      <h1>Welcome! Sign up here</h1>
+      {/* Form to sign up  component here*/}
+      {/* <form> */}
+    </>
+  );
+}

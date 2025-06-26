57bf26c8b90b4fb9241890cfd1e1ed25c8296b2d
Ross Fletcher <rossfletcher108@gmail.com>
2025-06-26 11:57:43 +0100

    initial setup

--- src/app/sign-up/[[...sign-up]]/createProfile/page.tsx
+++ src/app/sign-up/[[...sign-up]]/createProfile/page.tsx
@@ -1,19 +1,21 @@
-// TODO: Allow the user to add their personal information
-// clerkId does not exist unti lthe user signs up
+// // TODO: Allow the user to add their personal information
+// // clerkId does not exist unti lthe user signs up
+// // redirects are your best friend to set up a convenient user flow
+// //  - Clerk redirects are in the .env.local file
 
-export default function CreateProfilePage() {
-  // When you are handling the submit, you will store the users data into the users tabl;e
-  // as part of this insert, make sure you also add clerkId from auth()
+// export default function CreateProfilePage() {
+//   // When you are handling the submit, you will store the users data into the users tabl;e
+//   // as part of this insert, make sure you also add clerkId from auth()
 
-  //   if (!userId){
-  //     // prompt the user to sign up
-  //   }
+//   //   if (!userId){
+//   //     // prompt the user to sign up
+//   //   }
 
-  return (
-    <>
-      <h1>Welcome! Sign up here</h1>
-      {/* Form to sign up  component here*/}
-      {/* <form> */}
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h1>Welcome! Sign up here</h1>
+//       {/* Form to sign up  component here*/}
+//       {/* <form> */}
+//     </>
+//   );
+// }
